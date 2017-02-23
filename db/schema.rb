c9e56354a906fccd41a5b885c44d5d0b0b128adb
Caiua Franca <caiuagomes@gmail.com>
2017-02-23 15:58:31 -0300

    ajuste e adicionado tipo de contato

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170223125326) do
+ActiveRecord::Schema.define(version: 20170223165706) do
 
   create_table "contacts", force: :cascade do |t|
     t.string   "name"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170223125326) do
     t.date     "birthday"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "kind_id"
+    t.index ["kind_id"], name: "index_contacts_on_kind_id"
+  end
+
+  create_table "kinds", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
   end
 
 end
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -1,12 +1,24 @@
 namespace :dev do
   desc "Add contacts Faker"
   task setup: :environment do
+
+    puts "Criando Tipos de Contatos"
+    kinds = %w(Amigo Comercial Residencial)
+
+    kinds.each do |e|
+      Kind.create!(description: e)    
+    end 
+
+    puts "Criação concluida com Sucesso!"
+
+
     puts "Criando Contatos Faker"
     100.times do
       Contact.create!(
         name: Faker::Name.name,
         email: Faker::Internet.email,
-        birthday: Faker::Date.birthday
+        birthday: Faker::Date.birthday,
+        kind: Kind.all.sample
       )
     end
     puts "Criação concluida com Sucesso!"
